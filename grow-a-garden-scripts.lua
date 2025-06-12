90a4f4b7e05aa9a6dd2976e8c585ed2bb78ad468
loiscripts <kiel052609@gmail.com>
2025-06-13 06:21:23 +0800

    Update grow-a-garden-scripts.lua

--- grow-a-garden-scripts.lua
+++ grow-a-garden-scripts.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://paste.ee/r/WEDMIg6r"))()
---loadstring(game:HttpGet("https://raw.githubusercontent.com/loiscripts/Ray-Field/refs/heads/main/GAG-dupe-update"))()
+--loadstring(game:HttpGet("https://paste.ee/r/2HHX1Moe"))()
